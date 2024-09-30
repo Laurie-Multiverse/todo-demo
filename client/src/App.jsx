ddf38b70f6e3b2c8b85d5fa3154cba5c137de6bb
Laurie-Multiverse <laurie.corrin@multiverse.io>
2024-09-30 09:30:40 -0700

    added array.map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,15 +21,25 @@ function App() {
       description: "Log onto machine and open all relevant software",
       time: "9am",
     },
+    {
+      id: 4,
+      title: "Coffee Break",
+      description: "mmm good",
+      time: "10am"
+    }
   ];
   
   return (
     <>
       <h1>To Do List</h1>
       <ol>
-        <ListItem data={data[0]}/>
+        {/* map each task to a ListItem */}
+        {
+          data.map( task => ( <ListItem key={task.id} data={task}/> ))
+        }
+        {/* <ListItem data={data[0]}/>
         <ListItem data={data[1]}/>
-        <ListItem data={data[2]}/>
+        <ListItem data={data[2]}/> */}
       </ol>
     </>
   )
