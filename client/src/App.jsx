cc6b091c34014f39d57b7905a570bd4a06bfd07e
Laurie-Multiverse <laurie.corrin@multiverse.io>
2024-09-30 09:15:45 -0700

    add to do list with one item

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,14 @@ function App() {
  
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>To Do List</h1>
+      <ol>
+        <li>
+          <h2>Have Breakfast</h2>
+          <p>Description: 2 eggs on toast</p>
+          <p>Time: 7am</p>
+        </li>
+      </ol>
     </>
   )
 }
